4ba1a5a227e50a857c71db3f61939a630c48fafd
Aneesh <aneeshg2001@gmail.com>
2025-05-23 10:06:45 +0530

    new files added

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './LanguageSelector.css'; // optional: for styling
 
-function LanguageSelector() {
-  return (
-    <div>LanguageSelector</div>
-  )
+function LanguageSelector({ languages, selectedLang, onChange, label }) {
+    return (
+        <div className="language-selector">
+            {label && <label>{label}</label>}
+            <select value={selectedLang} onChange={(e) => onChange(e.target.value)}>
+                {
+                    languages.map((lang) => (
+                        <option key={lang.code} value={lang.code}>
+                            {lang.name}
+                        </option>
+                    ))
+                }
+            </select>
+        </div>
+
+    )
 }
 
 export default LanguageSelector
